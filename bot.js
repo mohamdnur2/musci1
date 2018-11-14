767f9d137464f68af51a5eb64a7446533064db07
mohamdnur2 <44902522+mohamdnur2@users.noreply.github.com>
2018-11-14 16:39:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -230,7 +230,7 @@ function play(guild, song) {
 }
  
 const adminprefix = "1vip";
-const devs = ['274923685985386496'];
+const devs = ['455108784579149835'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
